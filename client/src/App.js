4d656427a8dbb2522e5a7c17c288095b75853779
Adam K <krueng@gmail.com>
2021-10-28 15:00:31 -0500

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    fetch('/api/foo')
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {message}
+      </header>
+    </div>
+  );
+}
+
+export default App;
