7e439030b45f4f07c5bf80c5295115b0ce732221
Adam K <krueng@gmail.com>
2021-10-28 14:12:44 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const path = require('path')
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'client/build')))
+const PORT = process.env.PORT || 5000
+
+app.get('/api/foo', (req, res)=> {
+  res.json({ message: 'hello there!'})
+})
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
+})
